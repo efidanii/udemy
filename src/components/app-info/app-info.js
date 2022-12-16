706284d5327e59f9ac843b3c54b21fbf479dc8f4
efidanii <efidanii@yandex.ru>
2022-12-16 23:30:49 +0700

    final

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -6,7 +6,7 @@ const AppInfo = (props) => {
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании</h1>
-            <h2>Общее число сотрудников: {numberOfEmpolers-1}</h2>
+            <h2>Общее число сотрудников: {numberOfEmpolers}</h2>
             <h2>Премию получат: {nubmerOfIncreasedEmpolers}</h2>
         </div>
     )

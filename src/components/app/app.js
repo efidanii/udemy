2bee68d256c7f6c37c2f29fc481b937d6c364d73
efidanii <efidanii@yandex.ru>
2022-12-16 23:30:16 +0700

    final

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,9 @@ class App extends Component {
                 {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
                 {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
-            ]
+            ],
+            term: '',
+            filter: 'rise'
         }
         this.maxId = 4;
     }
@@ -27,6 +29,17 @@ class App extends Component {
                 data: data.filter(item => item.id !== id)
             }
         })
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise)
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000)
+            default:
+                return items
+        }
     }
 
     // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
@@ -59,13 +72,30 @@ class App extends Component {
     }
 
 
+    searchEmp = (items, term) => {
+        if (term.length === 0){
+            return items
+        }
 
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
 
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
 
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
 
     render() {
-      let numberOfEmpolers = this.state.data.length,
-          nubmerOfIncreasedEmpolers = this.state.data.filter(item => item.increase).length
+    const {data, term, filter} = this.state,
+          numberOfEmpolers = this.state.data.length,
+          nubmerOfIncreasedEmpolers = this.state.data.filter(item => item.increase).length,
+          visibleData = this.filterPost(this.searchEmp(data, term), filter)
+
         return (
             <div className="app">
                 <AppInfo 
@@ -73,14 +103,15 @@ class App extends Component {
                     nubmerOfIncreasedEmpolers={nubmerOfIncreasedEmpolers}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
                 
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProps={this.onToggleProps}/>
+                    <span style={{fontSize:40px}}></span>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
